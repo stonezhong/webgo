24cfb98f9cad1e234b2ae09cb0f052b3bad2dcb4
Stone Zhong <stonezhong@hotmail.com>
2017-04-24 23:13:01 -0700

    update

--- src/client/components/board.js
+++ src/client/components/board.js
@@ -1,15 +1,67 @@
 'use strict';
 
 import { Component } from 'rogueone';
-const template = `<div>this is a board</div>`;
+
+const COLOR_BLACK = Symbol('BLACK');
+const COLOR_WHITE = Symbol('WHITE');
+const COLOR_EMPTY = Symbol('EMPTY');
+
+const STONE_SIZE = 35;
+const GRID_SIZE = 40;
+const START_X = 6;
+const START_Y = 6;
+
+const x = 18;
+const y = 18;
+
+const template = `
+<div style="
+    width: 768px;
+    height: 768px;
+    background-image: url('images/board-1024x1024.png');
+    background-size: cover;
+    position: relative;
+">
+`;
+
 
 export default class Board extends Component {
     constructor(module, model, parentComponent) {
         super(module, model, parentComponent);
     }
 
+    stone(x, y, color) {
+        let imgSrc = "";
+        if (color === COLOR_BLACK) {
+            imgSrc = "images/black-stone.png";
+        } else if (color === COLOR_WHITE) {
+            imgSrc = "images/white-stone.png";
+        } else {
+            imgSrc = "";
+        }
+        return `
+            <img
+                src="${imgSrc}" 
+                style="
+                    left: ${START_X + x * GRID_SIZE}px; 
+                    top: ${START_Y + y * GRID_SIZE}px; 
+                    width: ${STONE_SIZE}px; 
+                    height: ${STONE_SIZE}px;
+                    display: block;
+                    position: absolute;
+            ">
+        `;
+    }
+
     render(element) {
         const $ = this.module.$;
-        return $(template)[0];
+        let t = template;
+        for (let x = 0; x < 19; x ++) {
+            for (let y = 0; y < 19; y ++) {
+                t += this.stone(x, y, ((x + y)%2 === 0)?COLOR_BLACK:COLOR_WHITE);
+            }
+        }
+        t += "</div>";
+        return $(t)[0];
     }
 }
